0af6e98848fe3816f0ee09df6aacdbeb7edd8c61
murloc <eshik3.11@gmail.com>
2023-09-25 22:02:26 +0700

    a line's model has been created (here it will be temporary...)

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class LineAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('id', 'title', 'number', 'color')
+    list_display_links = ('id', 'title',)
+    search_fields = ('title', 'number',)
+
+
+admin.site.register(Line, LineAdmin)
