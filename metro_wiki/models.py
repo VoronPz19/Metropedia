9a118ab844f5da52468dd9ac9e3078d6101d4c0a
murloc <eshik3.11@gmail.com>
2023-09-27 20:39:14 +0700

    the line model has been moved

--- metro_wiki/models.py
+++ metro_wiki/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from colorfield.fields import ColorField
 
-# Create your models here.
+
+class Line(models.Model):
+    title = models.CharField(max_length=100, blank=False, verbose_name='Название линий')
+    slug = models.CharField(max_length=100, blank=False, unique=True, verbose_name='Ссылка')
+    number = models.CharField(max_length=3, blank=True, verbose_name='Номер линий')
+    color = ColorField(default='#EF161E', verbose_name='Цвет линий')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Линия'
+        verbose_name_plural = 'Линий'
+        ordering = ['number']
