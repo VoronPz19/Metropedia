d57fe898476a35de608c2043b600361b4cb80e27
murloc <eshik3.11@gmail.com>
2023-09-27 20:15:00 +0700

    Added file "main_page/utils.py"

---
+++ main_page/utils.py
@@ -0,0 +1,21 @@
+from .models import *
+from django.db.models import Count
+
+menu = [{'title': 'Информация метрополитена', 'url_name': 'info'},
+        {'title': 'Новости метрополитена', 'url_name': 'blogs'},
+]
+
+
+class DataMixin:
+
+    def get_user_context(self, **kwargs):
+        context = kwargs
+
+        user_menu = menu.copy()
+        if not self.request.user.is_authenticated:
+            user_menu.pop(0)
+        context['menu'] = user_menu
+
+        if 'cat_selected' not in context:
+            context['cat_selected'] = 0
+        return context
