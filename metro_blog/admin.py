98455047888a969c4f8b6aca39d8151bc10f5ddc
murloc <eshik3.11@gmail.com>
2023-10-08 19:11:11 +0700

    loaded module CKEditor

--- metro_blog/admin.py
+++ metro_blog/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
 from .models import *
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
+
+
+class BlogAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Blog
+        fields = '__all__'
 
 
 class BlogAdmin(admin.ModelAdmin):
+    form = BlogAdminForm
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('id', 'title', 'time_created', 'image')
     list_display_links = ('id', 'title',)
     search_fields = ('title', 'content',)
+
 
 admin.site.register(Blog, BlogAdmin)
