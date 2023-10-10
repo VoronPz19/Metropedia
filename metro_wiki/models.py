00d6b9bd87464fa9e18d76478833ca799852c83d
murloc <eshik3.11@gmail.com>
2023-10-10 22:27:36 +0700

    The structure of the "City" and "Lines" models has been changed

--- metro_wiki/models.py
+++ metro_wiki/models.py
@@ -1,30 +1,23 @@
 from django.db import models
 from colorfield.fields import ColorField
+from django.urls import reverse
 
 
 class City(models.Model):
     title = models.CharField(max_length=100, blank=False, verbose_name='Название города')
     slug = models.CharField(max_length=100, blank=False, unique=True, verbose_name='Ссылка')
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = 'Город'
-        verbose_name_plural = 'Города'
-
-
-class CityInfo(models.Model):
-    city = models.OneToOneField(City, on_delete=models.PROTECT, verbose_name='линия')
     info = models.TextField(blank=True, null=True, verbose_name='Текст')
     image = models.ImageField(upload_to='images/%Y/%M/%D', blank=True, verbose_name='Картинка')
 
     def __str__(self):
-        return self.city
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('cities', kwargs={'cities_slug': self.slug})
 
     class Meta:
-        verbose_name = 'Информация о городе'
-        verbose_name_plural = 'Информация о городе'
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
 
 
 class Line(models.Model):
@@ -33,23 +26,15 @@ class Line(models.Model):
     number = models.CharField(max_length=3, blank=True, verbose_name='Номер линий')
     color = ColorField(default='#EF161E', verbose_name='Цвет линий')
     city = models.ForeignKey(City, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = 'Линия'
-        verbose_name_plural = 'Линии'
-
-
-class LineInfo(models.Model):
-    line = models.OneToOneField(Line, on_delete=models.PROTECT, verbose_name='линия')
     info = models.TextField(blank=True, null=True, verbose_name='Текст')
     image = models.ImageField(upload_to='images/%Y/%M/%D', blank=True, verbose_name='Картинка')
 
     def __str__(self):
-        return self.line
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('line', kwargs={'line_slug': self.slug})
 
     class Meta:
-        verbose_name = 'Информация о линий'
-        verbose_name_plural = 'Информация о линий'
+        verbose_name = 'Линия'
+        verbose_name_plural = 'Линии'
