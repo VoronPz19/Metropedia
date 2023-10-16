83e233210ceb81af03ec67f326f2f0328c3665c2
murloc <eshik3.11@gmail.com>
2023-10-16 23:11:33 +0700

    Added URL-address 'cities' (selected functional approach)

--- main_page/urls.py
+++ main_page/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
+from main_page import views
 
 urlpatterns = [
     path('', LastNews.as_view(), name='main'),
+    path('', views.cities, name='main_cities'),
 ]
