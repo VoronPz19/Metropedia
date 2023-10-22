facd4ce64f8d77542e437983aacbea1b1354faa2
murloc <eshik3.11@gmail.com>
2023-10-22 18:52:58 +0700

    -The "Cities" handler has been removed -Changed the template path for the "ShowCity" markup -The "Line" handler has been added

--- metro_wiki/views.py
+++ metro_wiki/views.py
@@ -1,26 +1,27 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import DetailView
 from .models import *
 from main_page.utils import DataMixin
 
 
-class Cities(DataMixin, ListView):
-    model = City
-    template_name = 'wiki/cities/index.html'
-    context_object_name = 'cities_list'
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title='Города')
-        return dict(list(context.items()) + list(c_def.items()))
-
-
 class ShowCity(DataMixin, DetailView):
     model = City
-    template_name = 'wiki/cities/city.html'
+    template_name = 'wiki/city.html'
     slug_url_kwarg = 'cities_slug'
     context_object_name = 'cities'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title=context['cities'])
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class ShowLine(DataMixin, DetailView):
+    model = Line
+    template_name = 'wiki/line.html'
+    slug_url_kwarg = 'line_slug'
+    context_object_name = 'line'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title=context['line'])
         return dict(list(context.items()) + list(c_def.items()))
