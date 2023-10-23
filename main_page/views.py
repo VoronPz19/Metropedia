bc5ec0086df463ed5cd68733c1682c777dbf95df
murloc <eshik3.11@gmail.com>
2023-10-23 20:12:34 +0700

    Added handler of the following elements: -RegisterUser -LoginUser -Account (Temporary stub)

--- main_page/views.py
+++ main_page/views.py
@@ -1,7 +1,13 @@
-from django.views.generic import ListView, DetailView
+from django.shortcuts import redirect
+from django.contrib.auth import login
+from django.views.generic import ListView, CreateView
 from metro_blog.models import *
 from metro_wiki.models import *
+from .forms import *
+from django.urls import reverse_lazy
 from main_page.utils import DataMixin, LastNewsMixin
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class LastNews(DataMixin, LastNewsMixin, ListView):
@@ -14,3 +20,41 @@ class LastNews(DataMixin, LastNewsMixin, ListView):
         c_def = self.get_user_context(title='Главная страница')
         return dict(list(context.items()) + list(c_def.items()))
 
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'main_page/register.html'
+    success_url = reverse_lazy('main')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('main')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = AuthenticationForm
+
+    template_name = 'main_page/login.html'
+    success_url = reverse_lazy('main')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class Account(DataMixin, ListView):
+    model = Blog
+    template_name = 'main_page/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Главная страница')
+        return dict(list(context.items()) + list(c_def.items()))
