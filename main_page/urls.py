f5dcafd4cc86eb73af682300fb39521e7407ccd1
murloc <eshik3.11@gmail.com>
2023-10-23 20:03:32 +0700

    Added URLs of the following elements: -register -login -logout -accounts/profile

--- main_page/urls.py
+++ main_page/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', LastNews.as_view(), name='main'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(template_name='main_page/logout.html'), name='logout'),
+    path('accounts/profile/', Account.as_view(), name='account'),
 ]
