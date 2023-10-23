3ae45814b7879288183621eeddc50c000ae3267a
murloc <eshik3.11@gmail.com>
2023-10-23 19:59:49 +0700

    -Fixed a bug with switching to page lines -Added URL "stations"

--- metro_wiki/urls.py
+++ metro_wiki/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('post/<slug:cities_slug>/', ShowCity.as_view(), name='cities'),
-    path('post/<slug:line_slug>/', ShowLine.as_view(), name='line'),
+    path('city/<slug:cities_slug>/', ShowCity.as_view(), name='cities'),
+    path('line/<slug:line_slug>/', ShowLine.as_view(), name='line'),
+    path('station/<slug:station_slug>/', ShowStation.as_view(), name='station'),
 ]
