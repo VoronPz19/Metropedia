a7894fd34b4a8a7d33d2522c25d033ebf130c3a9
murloc <eshik3.11@gmail.com>
2023-10-23 19:55:02 +0700

    file added "forms.py"

---
+++ main_page/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from metro_blog.models import *
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Подтверждение пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2',)
+
+
+class AddPostForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cat'].empty_label = 'Категория не выбрана'
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 200:
+            raise ValidationError('Длина превышает 200 символов')
+        return title
+
+    class Meta:
+        model = Blog
+        exclude = ['time_created', 'time_update']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'slug': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.TextInput(attrs={'class': 'form-input', 'cols': 60, 'rows': 10}),
+        }
