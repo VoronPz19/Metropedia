dd30eef20d0d621607c137cdc33458b929ea6564
murloc <eshik3.11@gmail.com>
2023-10-25 21:54:14 +0700

    -Added the handler "categories" (cities, lines, stations) - changed the handler "ShowCity"

--- metro_wiki/views.py
+++ metro_wiki/views.py
@@ -1,17 +1,50 @@
-from django.views.generic import DetailView
+from django.views.generic import ListView, DetailView
 from .models import *
 from main_page.utils import DataMixin
+
+
+class Cities(DataMixin, ListView):
+    model = City
+    template_name = 'wiki/categories/cities.html'
+    context_object_name = 'cities'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Города')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class Lines(DataMixin, ListView):
+    model = Line
+    template_name = 'wiki/categories/lines.html'
+    context_object_name = 'lines'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Линий')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class Stations(DataMixin, ListView):
+    model = Station
+    template_name = 'wiki/categories/stations.html'
+    context_object_name = 'stations'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Станций')
+        return dict(list(context.items()) + list(c_def.items()))
 
 
 class ShowCity(DataMixin, DetailView):
     model = City
     template_name = 'wiki/city.html'
-    slug_url_kwarg = 'cities_slug'
-    context_object_name = 'cities'
+    slug_url_kwarg = 'city_slug'
+    context_object_name = 'city'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title=context['cities'])
+        c_def = self.get_user_context(title=context['city'])
         return dict(list(context.items()) + list(c_def.items()))
 
 
