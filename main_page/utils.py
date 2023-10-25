cfd3826c31300100f2583bee77c24ae6d8123d72
murloc <eshik3.11@gmail.com>
2023-10-25 21:59:48 +0700

    - Now the menu no longer displays the "Metro Information" button - Added category utility

--- main_page/utils.py
+++ main_page/utils.py
@@ -1,6 +1,9 @@
-menu = [{'title': 'Информация метрополитена', 'url_name': 'main'},
-        {'title': 'Новости метрополитена', 'url_name': 'blogs'},
+menu = [{'title': 'Новости метрополитена', 'url_name': 'blogs'},
         {'title': 'Предложить новость', 'url_name': 'main'}]
+
+cats = [{'title': 'Города', 'url_name': 'cities', 'image': 'images/cities.jpg'},
+        {'title': 'Линий', 'url_name': 'lines', 'image': 'images/lines.jpg'},
+        {'title': 'Станций', 'url_name': 'stations', 'image': 'images/stations.jpg'}]
 
 
 class DataMixin:
@@ -12,6 +15,7 @@ class DataMixin:
         if not self.request.user.is_authenticated:
             user_menu.pop(-1)
         context['menu'] = user_menu
+        context['cats'] = cats
 
         if 'cat_selected' not in context:
             context['cat_selected'] = 0
