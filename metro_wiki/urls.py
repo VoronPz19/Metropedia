208f41e8ac4962133b185f9c36a129363c4ba945
murloc <eshik3.11@gmail.com>
2023-10-25 21:52:07 +0700

    -Added the URL for the "categories" (cities, lines, stations) - Changed the URL for the "city"

--- metro_wiki/urls.py
+++ metro_wiki/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('city/<slug:cities_slug>/', ShowCity.as_view(), name='cities'),
+    path('city/', Cities.as_view(), name='cities'),
+    path('line/', Lines.as_view(), name='lines'),
+    path('station/', Stations.as_view(), name='stations'),
+    path('city/<slug:city_slug>/', ShowCity.as_view(), name='city'),
     path('line/<slug:line_slug>/', ShowLine.as_view(), name='line'),
     path('station/<slug:station_slug>/', ShowStation.as_view(), name='station'),
 ]
