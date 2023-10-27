4a389909493003eac4d6f8642d0b458f1629ab26
murloc <eshik3.11@gmail.com>
2023-10-27 20:47:35 +0700

    User data has been transferred to the appropriate application

--- main_page/urls.py
+++ main_page/urls.py
@@ -4,8 +4,4 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', LastNews.as_view(), name='main'),
-    path('register/', RegisterUser.as_view(), name='register'),
-    path('login/', LoginUser.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(template_name='main_page/logout.html'), name='logout'),
-    path('accounts/profile/', Account.as_view(), name='account'),
 ]
