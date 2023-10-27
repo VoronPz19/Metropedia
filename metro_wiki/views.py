fdf6d33b8907271367f89c5166317121450cc40b
murloc <eshik3.11@gmail.com>
2023-10-27 16:45:58 +0700

    - Now stations get a list of stations on the line - The sorting of line numbers has been changed

--- metro_wiki/views.py
+++ metro_wiki/views.py
@@ -21,6 +21,9 @@ class Lines(DataMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['cities'] = City.objects.all
+        context['lines'] = Line.objects.extra(select={'sorted_num': 'CAST(number AS INTEGER)'})\
+            .order_by('sorted_num')
         c_def = self.get_user_context(title='Линий')
         return dict(list(context.items()) + list(c_def.items()))
 
@@ -44,6 +47,8 @@ class ShowCity(DataMixin, DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['lines'] = Line.objects.extra(select={'sorted_num': 'CAST(number AS INTEGER)'})\
+            .order_by('sorted_num')
         c_def = self.get_user_context(title=context['city'])
         return dict(list(context.items()) + list(c_def.items()))
 
@@ -68,5 +73,6 @@ class ShowStation(DataMixin, DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['station_list'] = Station.objects.all
         c_def = self.get_user_context(title=context['station'])
         return dict(list(context.items()) + list(c_def.items()))
