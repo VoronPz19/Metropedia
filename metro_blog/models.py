1ad3709237db38806ece86064f66c710cb8087d5
murloc <eshik3.11@gmail.com>
2023-10-28 15:45:55 +0700

    updated the model of posts

--- metro_blog/models.py
+++ metro_blog/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from metro_wiki.models import Line
 from django.urls import reverse
+from users.models import Profile
 
 
 class Blog(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.SET_NULL, blank=True, null=True)
     title = models.CharField(max_length=250, blank=False, verbose_name='Название')
     slug = models.CharField(max_length=250, unique=True, verbose_name='Ссылка')
     content = models.TextField(blank=True, null=True, verbose_name='Текст')
     source = models.CharField(max_length=250, blank=True, null=True, verbose_name='Источник')
     image = models.ImageField(upload_to='images/%Y/%M/%D', blank=True, verbose_name='Картинка')
+    is_public = models.BooleanField(default=False)
     time_created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
     line = models.ForeignKey(Line, on_delete=models.PROTECT, blank=True, null=True, verbose_name='линия')
