5651f23e853dcc697f06dbaa753c3ab23ab8e7e9
murloc <eshik3.11@gmail.com>
2023-10-28 15:38:25 +0700

    Additional contexts have been created for the "stations" handler

--- metro_wiki/views.py
+++ metro_wiki/views.py
@@ -35,6 +35,8 @@ class Stations(DataMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['cities'] = City.objects.all
+        context['stations'] = Station.objects.order_by('title', 'line')
         c_def = self.get_user_context(title='Станций')
         return dict(list(context.items()) + list(c_def.items()))
 
