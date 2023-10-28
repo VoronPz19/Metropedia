fa1a62469994da302b040ff476b73cd3861f22d5
murloc <eshik3.11@gmail.com>
2023-10-28 12:47:47 +0700

    The application and its "user" structure were created

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth import authenticate
+from django.contrib.auth.admin import UserAdmin
+from .forms import *
+
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = Profile
+
+    list_display = ('username', 'email', 'is_staff', 'is_active',)
+    list_filter = ('username', 'email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'image', 'is_staff', 'is_active')}
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(Profile, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from .manager import CustomUserManager
+from django.urls import reverse
 
-# Create your models here.
+
+class Profile(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=150, unique=True, blank=True)
+    email = models.EmailField(_('email_address'), unique=True, max_length=200)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='avatars/%Y/%M/%D', default='avatars/avatar.jpg', blank=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def get_absolute_url(self):
+        return reverse('profile', kwargs={'profile_pk': 'pk'})
+
+
+def is_staff(self):
+    return self.staff
+
+
+@property
+def is_admin(self):
+    return self.admin
+
+
+def __str__(self):
+    return self.email
