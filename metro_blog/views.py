52bd8df184f3185943f8d31d0e7ba16bc94b93fb
murloc <eshik3.11@gmail.com>
2023-10-28 18:07:25 +0700

    Now the site displays only those posts where the is_public value is True

--- metro_blog/views.py
+++ metro_blog/views.py
@@ -10,6 +10,7 @@ class Blogs(DataMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['posts'] = Blog.objects.filter(is_public=True)
         c_def = self.get_user_context(title='Новости')
         return dict(list(context.items()) + list(c_def.items()))
 
