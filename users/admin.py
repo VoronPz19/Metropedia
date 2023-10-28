c365be6334f38cfc1c3fe92673bd27fa845c53c8
murloc <eshik3.11@gmail.com>
2023-10-28 13:30:54 +0700

    minor changes

--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,6 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import *
 
 
-# Register your models here.
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
