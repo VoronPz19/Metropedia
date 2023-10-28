acdbd3e521026a42faee90a2336feef3e00b2c62
murloc <eshik3.11@gmail.com>
2023-10-28 12:57:56 +0700

    The application and its "user" structure were created

--- metropedia/settings.py
+++ metropedia/settings.py
@@ -118,6 +118,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+# User
+AUTH_USER_MODEL = 'users.Profile'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
