bf2903bc55341f0197f82ee101b35d384826fbc0
murloc <eshik3.11@gmail.com>
2023-10-29 12:22:30 +0700

    Added URL "add_station"

--- metro_wiki/urls.py
+++ metro_wiki/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
     path('city/', Cities.as_view(), name='cities'),
     path('line/', Lines.as_view(), name='lines'),
     path('station/', Stations.as_view(), name='stations'),
+
     path('city/<slug:city_slug>/', ShowCity.as_view(), name='city'),
     path('line/<slug:line_slug>/', ShowLine.as_view(), name='line'),
     path('station/<slug:station_slug>/', ShowStation.as_view(), name='station'),
+
+    path('station/add/station', AddStation.as_view(), name='add_station'),
 ]
