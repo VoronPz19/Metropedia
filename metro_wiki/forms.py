102ee7658e8dcf5284e388ef14eef59551208d7f
murloc <eshik3.11@gmail.com>
2023-10-29 12:20:47 +0700

    Added file "forms.py"

---
+++ metro_wiki/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.forms import ModelForm, CheckboxSelectMultiple, RadioSelect
+from .models import *
+
+
+class AddStationForm(ModelForm):
+    class Meta:
+        model = Station
+        fields = '__all__'
+        widgets = {
+            'transfer': CheckboxSelectMultiple(),
+            'line': RadioSelect(),
+            'title': forms.TextInput(),
+            'slug': forms.TextInput(),
+            'content': forms.TextInput(attrs={'cols': 60, 'rows': 10}),
+                   }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-input'})
+
+        self.fields['content'].widget.attrs.update({'class': 'form-control django_ckeditor_5'})
+        self.fields['content'].required = False
