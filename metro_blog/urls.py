047a1a6ec3d734307771555bf5251a1a1c929e09
murloc <eshik3.11@gmail.com>
2023-10-29 19:51:46 +0700

    Added URL "add_post"

--- metro_blog/urls.py
+++ metro_blog/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', Blogs.as_view(), name='blogs'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
+    path('add_post/', AddPost.as_view(), name='add_post'),
 ]
