cb321f8dca9217ea5e897e602eddc9340f5ac6d4
murloc <eshik3.11@gmail.com>
2023-10-29 19:52:06 +0700

    Added handler "AddPost"

--- metro_blog/views.py
+++ metro_blog/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView, DetailView
-from .models import *
+from django.views.generic import ListView, DetailView, CreateView
+from .forms import *
+from django.urls import reverse_lazy
 from main_page.utils import DataMixin
 
 
@@ -24,4 +25,16 @@ class ShowPost(DataMixin, DetailView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title=context['post'])
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class AddPost(DataMixin, CreateView):
+    form_class = AddPostForm
+    template_name = 'wiki/forms/station.html'
+    success_url = reverse_lazy('blogs')
+    login_url = reverse_lazy('main')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Конструктор новостей')
         return dict(list(context.items()) + list(c_def.items()))
