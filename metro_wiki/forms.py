978b5aa35b90a9b32b3379d2cc7a677accbf423a
murloc <eshik3.11@gmail.com>
2023-11-02 14:21:12 +0700

    Added "AddCityForm" and "AddLineForm" forms

--- metro_wiki/forms.py
+++ metro_wiki/forms.py
@@ -3,6 +3,46 @@ from django.forms import ModelForm, CheckboxSelectMultiple, RadioSelect
 from .models import *
 
 
+class AddCityForm(ModelForm):
+    class Meta:
+        model = City
+        fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(),
+            'slug': forms.TextInput(),
+            'content': forms.TextInput(attrs={'cols': 60, 'rows': 10}),
+                   }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-input'})
+
+        self.fields['content'].widget.attrs.update({'class': 'form-control django_ckeditor_5'})
+        self.fields['content'].required = False
+
+
+class AddLineForm(ModelForm):
+    class Meta:
+        model = Line
+        fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(),
+            'slug': forms.TextInput(),
+            'content': forms.TextInput(attrs={'cols': 60, 'rows': 10}),
+                   }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-input'})
+
+        self.fields['content'].widget.attrs.update({'class': 'form-control django_ckeditor_5'})
+        self.fields['content'].required = False
+
+
 class AddStationForm(ModelForm):
     class Meta:
         model = Station
