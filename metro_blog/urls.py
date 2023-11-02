14c80cc0093fb79e5a79b71c04bd2267ba3b9ee9
murloc <eshik3.11@gmail.com>
2023-11-02 14:15:11 +0700

    Added new URLs

--- metro_blog/urls.py
+++ metro_blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', Blogs.as_view(), name='blogs'),
+    path('', PublicPosts.as_view(), name='blogs'),
+    path('post/special/list/', AllPosts.as_view(), name='post_list'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
-    path('add_post/', AddPost.as_view(), name='add_post'),
+    path('post/<slug:post_slug>/add_comment/', CommentCreateView.as_view(), name='add_comment'),
+    path('post/special/add/', AllPosts.as_view(), name='add_post'),
 ]
