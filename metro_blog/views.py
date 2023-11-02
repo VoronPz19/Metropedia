93283333924e66f917fa0e00a263ec1164154afd
murloc <eshik3.11@gmail.com>
2023-11-02 14:19:27 +0700

    Added the "AllPosts" class

--- metro_blog/views.py
+++ metro_blog/views.py
@@ -16,6 +16,17 @@ class PublicPosts(DataMixin, ListView):
         return dict(list(context.items()) + list(c_def.items()))
 
 
+class AllPosts(DataMixin, ListView):
+    model = Blog
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Предложенные новости')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
 class ShowPost(DataMixin, DetailView):
     model = Blog
     template_name = 'blog/post.html'
