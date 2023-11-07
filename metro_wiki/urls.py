9b74c2d4af3f422610f05a70a6e527bff83d756b
murloc <eshik3.11@gmail.com>
2023-11-07 20:38:47 +0700

    Added URL of trains and depots

--- metro_wiki/urls.py
+++ metro_wiki/urls.py
@@ -13,8 +13,12 @@ urlpatterns = [
     path('city/special/add/', AddCity.as_view(), name='add_city'),
     path('line/special/add/', AddLine.as_view(), name='add_line'),
     path('station/special/add/', AddStation.as_view(), name='add_station'),
+    path('train/special/add/', AddLine.as_view(), name='add_train'),
+    path('depot/special/add/', AddStation.as_view(), name='add_depot'),
 
     path('city/<slug:city_slug>/update/', UpdateCity.as_view(), name='update_city'),
     path('line/<slug:line_slug>/update/', UpdateLine.as_view(), name='update_line'),
     path('station/<slug:station_slug>/update/', UpdateStation.as_view(), name='update_station'),
+    path('train/<slug:line_slug>/update/', UpdateLine.as_view(), name='update_train'),
+    path('depot/<slug:station_slug>/update/', UpdateStation.as_view(), name='update_depot'),
 ]
