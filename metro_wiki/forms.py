e9d9bb3f40d79cfd7d376c1580e3c1d4d488541f
murloc <eshik3.11@gmail.com>
2023-11-07 20:33:41 +0700

    Added forms for trains and depots

--- metro_wiki/forms.py
+++ metro_wiki/forms.py
@@ -63,3 +63,46 @@ class AddStationForm(ModelForm):
 
         self.fields['content'].widget.attrs.update({'class': 'form-control django_ckeditor_5'})
         self.fields['content'].required = False
+
+
+class AddTrainForm(ModelForm):
+    class Meta:
+        model = Line
+        fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(),
+            'slug': forms.TextInput(),
+            'content': forms.TextInput(attrs={'cols': 60, 'rows': 10}),
+                   }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-input'})
+
+        self.fields['content'].widget.attrs.update({'class': 'form-control django_ckeditor_5'})
+        self.fields['content'].required = False
+
+
+class AddDepotForm(ModelForm):
+    class Meta:
+        model = Depot
+        fields = '__all__'
+        widgets = {
+            'trains': CheckboxSelectMultiple(),
+            'lines': CheckboxSelectMultiple(),
+            'city': RadioSelect(),
+            'title': forms.TextInput(),
+            'slug': forms.TextInput(),
+            'content': forms.TextInput(attrs={'cols': 60, 'rows': 10}),
+                   }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-input'})
+
+        self.fields['content'].widget.attrs.update({'class': 'form-control django_ckeditor_5'})
+        self.fields['content'].required = False
