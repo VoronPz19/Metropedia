f9ba3b4cac5e31ac57e4f84a47088f0a97605748
murloc <eshik3.11@gmail.com>
2023-11-08 22:07:46 +0700

    - Added handlers for trains and depots - The paths to the objects templates have been changed - Added additional contexts in existing handlers - Added separation of handlers (this does not affect the site in any way)

--- metro_wiki/views.py
+++ metro_wiki/views.py
@@ -4,6 +4,8 @@ from metro_blog.models import Blog
 from django.urls import reverse_lazy
 from main_page.utils import DataMixin
 
+# -- Categories --
+
 
 class Cities(DataMixin, ListView):
     model = City
@@ -46,9 +48,36 @@ class Stations(DataMixin, ListView):
         return dict(list(context.items()) + list(c_def.items()))
 
 
+class Trains(DataMixin, ListView):
+    model = Train
+    template_name = 'wiki/categories/trains.html'
+    context_object_name = 'trains'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
+        c_def = self.get_user_context(title='Поезда')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class Depots(DataMixin, ListView):
+    model = Depot
+    template_name = 'wiki/categories/depots.html'
+    context_object_name = 'depots'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
+        c_def = self.get_user_context(title='Депо')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+# -- Objects --
+
+
 class ShowCity(DataMixin, DetailView):
     model = City
-    template_name = 'wiki/city.html'
+    template_name = 'wiki/objects/city.html'
     slug_url_kwarg = 'city_slug'
     context_object_name = 'city'
 
@@ -63,29 +92,61 @@ class ShowCity(DataMixin, DetailView):
 
 class ShowLine(DataMixin, DetailView):
     model = Line
-    template_name = 'wiki/line.html'
+    template_name = 'wiki/objects/line.html'
     slug_url_kwarg = 'line_slug'
     context_object_name = 'line'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-
+        context['depot'] = Depot.objects.all()
+        context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
         c_def = self.get_user_context(title=context['line'])
         return dict(list(context.items()) + list(c_def.items()))
 
 
 class ShowStation(DataMixin, DetailView):
     model = Station
-    template_name = 'wiki/station.html'
+    template_name = 'wiki/objects/station.html'
     slug_url_kwarg = 'station_slug'
     context_object_name = 'station'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['station_list'] = Station.objects.all()
+        context['depot'] = Depot.objects.all()
         context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
         c_def = self.get_user_context(title=context['station'])
         return dict(list(context.items()) + list(c_def.items()))
+
+
+class ShowTrain(DataMixin, DetailView):
+    model = Train
+    template_name = 'wiki/objects/train.html'
+    slug_url_kwarg = 'train_slug'
+    context_object_name = 'train'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        c_def = self.get_user_context(title=context['train'])
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class ShowDepot(DataMixin, DetailView):
+    model = Depot
+    template_name = 'wiki/objects/depot.html'
+    slug_url_kwarg = 'depot_slug'
+    context_object_name = 'depot'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['station_list'] = Station.objects.all()
+        context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
+        c_def = self.get_user_context(title=context['depot'])
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+# -- Adding objects --
 
 
 class AddCity(DataMixin, CreateView):
@@ -96,7 +157,7 @@ class AddCity(DataMixin, CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
-        c_def = self.get_user_context(title='Добавить/редактировать город')
+        c_def = self.get_user_context(title='Добавить город')
         return dict(list(context.items()) + list(c_def.items()))
 
 
@@ -108,7 +169,7 @@ class AddLine(DataMixin, CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
-        c_def = self.get_user_context(title='Добавить/редактировать линию')
+        c_def = self.get_user_context(title='Добавить линию')
         return dict(list(context.items()) + list(c_def.items()))
 
 
@@ -120,8 +181,35 @@ class AddStation(DataMixin, CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
-        c_def = self.get_user_context(title='Добавить/редактировать станцию')
+        c_def = self.get_user_context(title='Добавить станцию')
         return dict(list(context.items()) + list(c_def.items()))
+
+
+class AddTrain(DataMixin, CreateView):
+    form_class = AddTrainForm
+    template_name = 'main_page/forms.html'
+    success_url = reverse_lazy('trains')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
+        c_def = self.get_user_context(title='Добавить метровагон')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class AddDepot(DataMixin, CreateView):
+    form_class = AddDepotForm
+    template_name = 'main_page/forms.html'
+    success_url = reverse_lazy('depots')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
+        c_def = self.get_user_context(title='Добавить депо')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+# -- Editing objects --
 
 
 class UpdateCity(DataMixin, UpdateView):
@@ -145,7 +233,7 @@ class UpdateCity(DataMixin, UpdateView):
 
 
 class UpdateLine(DataMixin, UpdateView):
-    model = City
+    model = Line
     form_class = AddLineForm
     template_name = 'main_page/forms.html'
     slug_url_kwarg = 'line_slug'
@@ -165,7 +253,7 @@ class UpdateLine(DataMixin, UpdateView):
 
 
 class UpdateStation(DataMixin, UpdateView):
-    model = City
+    model = Station
     form_class = AddStationForm
     template_name = 'main_page/forms.html'
     slug_url_kwarg = 'station_slug'
@@ -178,6 +266,46 @@ class UpdateStation(DataMixin, UpdateView):
         update = True
         context['update'] = update
         c_def = self.get_user_context(title='Редактировать станцию')
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_queryset(self):
+        return self.model.objects.all()
+
+
+class UpdateTrain(DataMixin, UpdateView):
+    model = Train
+    form_class = AddTrainForm
+    template_name = 'main_page/forms.html'
+    slug_url_kwarg = 'train_slug'
+
+    success_url = reverse_lazy('trains')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
+        update = True
+        context['update'] = update
+        c_def = self.get_user_context(title='Редактировать поезд')
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_queryset(self):
+        return self.model.objects.all()
+
+
+class UpdateDepot(DataMixin, UpdateView):
+    model = Depot
+    form_class = AddDepotForm
+    template_name = 'main_page/forms.html'
+    slug_url_kwarg = 'depot_slug'
+
+    success_url = reverse_lazy('depots')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
+        update = True
+        context['update'] = update
+        c_def = self.get_user_context(title='Редактировать депо')
         return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
