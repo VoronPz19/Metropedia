51a708c7f0f200fa8b5bd53fa245323466e11d94
murloc <eshik3.11@gmail.com>
2023-11-13 23:10:13 +0700

    The add news tab has been moved below

--- main_page/utils.py
+++ main_page/utils.py
@@ -2,12 +2,12 @@ menu = [{'title': 'Главная страница', 'url_name': 'main'},
         {'title': 'Новости метрополитена', 'url_name': 'blogs'}]
 
 submenu_editor = [{'title': 'Добавить город', 'url_name': 'add_city'},
-           {'title': 'Добавить линию', 'url_name': 'add_line'},
-           {'title': 'Добавить станцию', 'url_name': 'add_station'},
-           {'title': 'Добавить новость', 'url_name': 'add_post'},
-           {'title': 'Добавить Состав', 'url_name': 'add_train'},
-           {'title': 'Добавить Депо', 'url_name': 'add_depot'},
-           {'title': 'Посмотреть предложенные новости', 'url_name': 'post_list'}]
+                  {'title': 'Добавить линию', 'url_name': 'add_line'},
+                  {'title': 'Добавить станцию', 'url_name': 'add_station'},
+                  {'title': 'Добавить Состав', 'url_name': 'add_train'},
+                  {'title': 'Добавить Депо', 'url_name': 'add_depot'},
+                  {'title': 'Добавить новость', 'url_name': 'add_post'},
+                  {'title': 'Посмотреть предложенные новости', 'url_name': 'post_list'}]
 
 cats = [{'title': 'Города', 'url_name': 'cities', 'image': 'images/cities.jpg'},
         {'title': 'Линий', 'url_name': 'lines', 'image': 'images/lines.jpg'},
