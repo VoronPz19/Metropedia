3ff65e187f1ad834fe796752af9a9542465c8833
murloc <eshik3.11@gmail.com>
2023-11-13 21:07:17 +0700

    Some URLs have been changed

--- users/urls.py
+++ users/urls.py
@@ -3,12 +3,12 @@ from .views import *
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('accounts/profile/<str:pk>', Account.as_view(), name='user_profile'),
+    path('profile/<str:profile_pk>/', Account.as_view(), name='user_profile'),
+    path('profile/<str:profile_pk>/edit', EditUser.as_view(), name='user_profile_edit'),
 
     path('register/', RegisterUser.as_view(), name='register'),
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', LogoutView.as_view(template_name='user/logout.html'), name='logout'),
 
-    path('accounts/profile/you/', Account.as_view(), name='account'),
-    path('accounts/profile/you/edit', Account.as_view(), name='edit_account'),
+    path('account/', UserAccount.as_view(), name='user'),
 ]
