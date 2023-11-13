11f2b78fb6819f0a086e6613092dfe459a2b7688
murloc <eshik3.11@gmail.com>
2023-11-13 21:06:10 +0700

    - Added account handlers and its editing - The account system has been improved

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect
 from django.contrib.auth import login
-from django.views.generic import ListView, CreateView
+from django.views.generic import DetailView, CreateView, UpdateView, ListView
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -13,7 +13,7 @@ from main_page.utils import DataMixin
 class RegisterUser(DataMixin, CreateView):
     form_class = RegisterUserForm
     template_name = 'user/register.html'
-    success_url = reverse_lazy('account')
+    success_url = reverse_lazy('user')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -22,17 +22,31 @@ class RegisterUser(DataMixin, CreateView):
         c_def = self.get_user_context(title='Регистрация')
         return dict(list(context.items()) + list(c_def.items()))
 
+
+class EditUser(LoginRequiredMixin, DataMixin, UpdateView):
+    model = Profile
+    template_name = 'user/user_edit_form.html'
+    pk_url_kwarg = 'profile_pk'
+    form_class = EditingUserForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
+        context['form'] = EditingUserForm(instance=self.request.user)
+        c_def = self.get_user_context(title='Настройки аккаунта')
+        return dict(list(context.items()) + list(c_def.items()))
+
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect('account')
+        return redirect('user')
 
 
 class LoginUser(DataMixin, LoginView):
     form_class = AuthenticationUserForm
-
     template_name = 'user/login.html'
-    success_url = reverse_lazy('account')
+    success_url = reverse_lazy('user')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -42,18 +56,44 @@ class LoginUser(DataMixin, LoginView):
         return dict(list(context.items()) + list(c_def.items()))
 
 
-class Account(LoginRequiredMixin, DataMixin, ListView):
+class Account(LoginRequiredMixin, DataMixin, DetailView):
     model = Profile
+    pk_url_kwarg = 'profile_pk'
     template_name = 'user/profile.html'
     context_object_name = 'user'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['post_user'] = Blog.objects.filter(owner=self.request.user)
+
         if self.request.user.is_authenticated:
             context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
+
         # ppuc =(public post user count)
         context['ppuc'] = Blog.objects.filter(owner=self.request.user, is_public=True).count
+        context['post_user'] = Blog.objects.filter(owner=self.request.user)
+        context['tracked_city_posts'] = Blog.objects.filter(city=self.request.user.tracked_city)
         context['posts'] = Blog.objects.filter(is_public=True)
-        c_def = self.get_user_context(title='Главная страница')
+
+        c_def = self.get_user_context(title=self.request.user)
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class UserAccount(LoginRequiredMixin, DataMixin, ListView):
+    model = Profile
+    template_name = 'user/account.html'
+    context_object_name = 'user'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+            context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
+
+        # ppuc =(public post user count)
+        context['ppuc'] = Blog.objects.filter(owner=self.request.user, is_public=True).count
+        context['post_user'] = Blog.objects.filter(owner=self.request.user)
+        context['tracked_city_posts'] = Blog.objects.filter(city=self.request.user.tracked_city)
+        context['posts'] = Blog.objects.filter(is_public=True)
+
+        c_def = self.get_user_context(title=self.request.user)
         return dict(list(context.items()) + list(c_def.items()))
