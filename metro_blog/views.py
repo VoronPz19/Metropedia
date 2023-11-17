ad883a1b3429fcb518cb220cc6726d3d345c42ec
murloc <eshik3.11@gmail.com>
2023-11-17 23:29:10 +0700

    Added the publishing feature/removing the post

--- metro_blog/views.py
+++ metro_blog/views.py
@@ -85,16 +85,21 @@ class AddComment(LoginRequiredMixin, DataMixin, CreateView):
 class AddPost(LoginRequiredMixin, DataMixin, CreateView):
     template_name = 'main_page/forms.html'
     form_class = AddPostForm
-    success_url = reverse_lazy('post')
+
+    success_url = reverse_lazy('blogs')
+    login_url = reverse_lazy('login')
 
     def form_valid(self, form):
         new_post = form.save(commit=False)
         new_post.owner = self.request.user
+        if 'save_public' in self.request.POST:
+            new_post.is_public = True
         new_post.save()
         return super().form_valid(form)
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['public_form'] = True
         c_def = self.get_user_context(title='Конструктор новостей')
         return dict(list(context.items()) + list(c_def.items()))
 
@@ -104,9 +109,19 @@ class UpdatePost(LoginRequiredMixin, DataMixin, UpdateView):
     form_class = AddPostForm
     template_name = 'main_page/forms.html'
     slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
 
     success_url = reverse_lazy('blogs')
     login_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        new_post = form.save(commit=False)
+        if 'save_non_public' in self.request.POST:
+            new_post.is_public = False
+        if 'save_public' in self.request.POST:
+            new_post.is_public = True
+        new_post.save()
+        return super().form_valid(form)
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -114,26 +129,7 @@ class UpdatePost(LoginRequiredMixin, DataMixin, UpdateView):
             context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
         update = True
         context['update'] = update
-        c_def = self.get_user_context(title='Конструктор новостей')
-        return dict(list(context.items()) + list(c_def.items()))
-
-    def get_queryset(self):
-        return self.model.objects.filter(owner=self.request.user)
-
-
-class PublicPost(LoginRequiredMixin, DataMixin, UpdateView):
-    model = Blog
-    slug_url_kwarg = 'post_slug'
-    extra_context = {'post': Blog.objects.all()}
-    success_url = reverse_lazy('blogs')
-    login_url = reverse_lazy('login')
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if self.request.user.is_authenticated:
-            context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
-        update = True
-        context['update'] = update
+        context['public_form'] = True
         c_def = self.get_user_context(title='Конструктор новостей')
         return dict(list(context.items()) + list(c_def.items()))
 
