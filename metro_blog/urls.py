23f707059497f35f91165811b6d55b1c3ce346ff
murloc <eshik3.11@gmail.com>
2023-11-18 00:08:37 +0700

    Removed URL for comments

--- metro_blog/urls.py
+++ metro_blog/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('post/special/user_list/', UserPosts.as_view(), name='post_user_list'),
 
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
-    path('post/<slug:post_slug>/comment', AddComment.as_view(), name='post_comment'),
 
     path('post/special/add/', AddPost.as_view(), name='add_post'),
     path('post/<slug:post_slug>/update/', UpdatePost.as_view(), name='update_post'),
