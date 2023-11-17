16c422059960cae9a1ae945ef134c0c7030a0596
murloc <eshik3.11@gmail.com>
2023-11-17 23:23:34 +0700

    The method of displaying the number of posts on 1 page has been changed

--- main_page/views.py
+++ main_page/views.py
@@ -1,17 +1,18 @@
 from django.views.generic import ListView
 from metro_blog.models import *
-from main_page.utils import DataMixin, LastNewsMixin
+from main_page.utils import DataMixin
 
 
-class LastNews(DataMixin, LastNewsMixin, ListView):
+class LastNews(DataMixin, ListView):
     model = Blog
     template_name = 'main_page/index.html'
     context_object_name = 'posts'
+    view_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['posts'] = Blog.objects.filter(is_public=True)
-        context['count'] = Blog.objects.filter(is_public=True).count
+        context['posts'] = Blog.objects.filter(is_public=True)[:self.view_by]
+        context['count'] = Blog.objects.filter(is_public=True)[:self.view_by].count
         if self.request.user.is_authenticated:
             context['post_user_count'] = Blog.objects.filter(owner=self.request.user).count
         c_def = self.get_user_context(title='Главная страница')
