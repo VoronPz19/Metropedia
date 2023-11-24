0ff97926270f4bb3d1c727bcd7ee6c1d05fcc115
murloc <eshik3.11@gmail.com>
2023-11-24 23:14:03 +0700

    Added a new replaceable value

--- main_page/templatetags/blog_filters.py
+++ main_page/templatetags/blog_filters.py
@@ -6,7 +6,9 @@ register = template.Library()
 
 @register.filter
 def replacer(text):
-    filtration = {'&ndash;': '-', '&laquo;': '«', '&raquo;': '»', '&nbsp;': ' \n'}
+    filtration = {'&ndash;': '-', '&mdash;': '—',
+                  '&laquo;': '«', '&raquo;': '»',
+                  '&nbsp;': '\n '}
 
     for key, value in filtration.items():
         text = text.replace(key, value)
