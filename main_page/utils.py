d810654df288bb147793dbd420952e1702eb47bc
murloc <eshik3.11@gmail.com>
2023-11-24 23:17:15 +0700

    Added separator lines for the staff submenu

--- main_page/utils.py
+++ main_page/utils.py
@@ -14,13 +14,16 @@ def unique_slugify(instance, slug):
 menu = [{'title': 'Главная страница', 'url_name': 'main'},
         {'title': 'Новости метрополитена', 'url_name': 'blogs'}]
 
-submenu_editor = [{'title': 'Добавить город', 'url_name': 'add_city'},
+submenu_editor = [{'title': '__object_line_hr__'},
+                  {'title': 'Добавить город', 'url_name': 'add_city'},
                   {'title': 'Добавить линию', 'url_name': 'add_line'},
                   {'title': 'Добавить станцию', 'url_name': 'add_station'},
                   {'title': 'Добавить Состав', 'url_name': 'add_train'},
                   {'title': 'Добавить Депо', 'url_name': 'add_depot'},
+                  {'title': '__object_line_hr__'},
                   {'title': 'Добавить новость', 'url_name': 'add_post'},
-                  {'title': 'Посмотреть предложенные новости', 'url_name': 'post_list'}]
+                  {'title': 'Посмотреть предложенные новости', 'url_name': 'post_list'},
+                  {'title': '__object_line_hr__'},]
 
 cats = [{'title': 'Города', 'url_name': 'cities', 'image': 'images/cities.jpg'},
         {'title': 'Линий', 'url_name': 'lines', 'image': 'images/lines.jpg'},
