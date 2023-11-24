420270a3b8ffd1a667e05d06cdf223f1b0a25401
murloc <eshik3.11@gmail.com>
2023-11-24 23:15:24 +0700

    Added slug processing

--- main_page/utils.py
+++ main_page/utils.py
@@ -1,3 +1,16 @@
+from uuid import uuid4
+from pytils.translit import slugify
+
+
+def unique_slugify(instance, slug):
+    model = instance.__class__
+    unique_slug = slugify(slug)
+
+    while model.objects.filter(slug=unique_slug).exists():
+        unique_slug = f'{unique_slug}-{uuid4().hex[:8]}'
+    return unique_slug
+
+
 menu = [{'title': 'Главная страница', 'url_name': 'main'},
         {'title': 'Новости метрополитена', 'url_name': 'blogs'}]
 
